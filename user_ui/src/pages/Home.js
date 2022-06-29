09b41cc2968a560ed8a6bf0ecd1a3c40ffb6da6f
Filip Joksovic <filip.joksovic@student.um.si>
2022-06-29 14:40:11 +0200

    React started

---
+++ user_ui/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Home</h2>
+                <span>Cras facilisis urna ornare ex volutpat, et
+                convallis erat elementum. Ut aliquam, ipsum vitae
+                gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+                metus nec massa. Maecenas hendrerit laoreet augue
+                nec molestie. Cum sociis natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus.
+                </span>
+            </div>
+
+        );
+    }
+}
+export default Home;
